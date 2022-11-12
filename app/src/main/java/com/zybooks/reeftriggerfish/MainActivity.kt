ebaa25351870863300403d7e8e3f8e03f31829a0
d-bev <dmbevi29@gmail.com>
2022-11-12 14:07:01 -0800

    3. Users will be able to remove pieces by making matches
    (currently only row/col of 3)

--- app/src/main/java/com/zybooks/reeftriggerfish/MainActivity.kt
+++ app/src/main/java/com/zybooks/reeftriggerfish/MainActivity.kt
@@ -7,11 +7,12 @@ import android.util.DisplayMetrics
 import android.widget.ImageView
 import android.widget.TextView
 import com.zybooks.reeftriggerfish.uitel.OnSwipeListener
+import java.util.Arrays.asList
 
 class MainActivity : AppCompatActivity() {
 
     // Adding candies
-    var candyImages = intArrayOf(
+    var cellImages = intArrayOf(
         R.drawable.bluecandy,
         R.drawable.greencandy,
         R.drawable.orangecandy,
@@ -20,18 +21,19 @@ class MainActivity : AppCompatActivity() {
         R.drawable.purplecandy
     )
 
-    var blockWidth : Int = 0
-    var numBlocks : Int = 8
+    var cellWidth : Int = 0
+    var numCells : Int = 8
     var screenWidth : Int = 0
-    lateinit var candy : ArrayList<ImageView>
+    lateinit var cell : ArrayList<ImageView>
 
-    var candyToBeDragged : Int = 0
-    var candyToBeReplaced : Int = 0
-    var emptyBlock : Int = R.drawable.transparent
+    var cellToBeDragged : Int = 0
+    var cellToBeReplaced : Int = 0
+    var emptyCell : Int = R.drawable.transparent
 
     lateinit var mouseHandler: Handler
     private lateinit var scoreResult : TextView
     var score : Int = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -45,36 +47,36 @@ class MainActivity : AppCompatActivity() {
 
         var screenHeight = displayMetrics.heightPixels
 
-        blockWidth = screenWidth / numBlocks
+        cellWidth = screenWidth / numCells
 
-        candy = ArrayList()
+        cell = ArrayList()
         createBoard()
 
-        for (imageView in candy){
+        for (imageView in cell){
             imageView.setOnTouchListener(object : OnSwipeListener(this){
                 override fun onSwipeRight() {
                     super.onSwipeRight()
-                    candyToBeDragged = imageView.id
-                    candyToBeReplaced = candyToBeDragged + 1
-                    candySwap()
+                    cellToBeDragged = imageView.id
+                    cellToBeReplaced = cellToBeDragged + 1
+                    swapCells()
                 }
                 override fun onSwipeLeft() {
                     super.onSwipeLeft()
-                    candyToBeDragged = imageView.id
-                    candyToBeReplaced = candyToBeDragged - 1
-                    candySwap()
+                    cellToBeDragged = imageView.id
+                    cellToBeReplaced = cellToBeDragged - 1
+                    swapCells()
                 }
                 override fun onSwipeTop() {
                     super.onSwipeTop()
-                    candyToBeDragged = imageView.id
-                    candyToBeReplaced = candyToBeDragged - numBlocks
-                    candySwap()
+                    cellToBeDragged = imageView.id
+                    cellToBeReplaced = cellToBeDragged - numCells
+                    swapCells()
                 }
                 override fun onSwipeBottom() {
                     super.onSwipeBottom()
-                    candyToBeDragged = imageView.id
-                    candyToBeReplaced = candyToBeDragged + numBlocks
-                    candySwap()
+                    cellToBeDragged = imageView.id
+                    cellToBeReplaced = cellToBeDragged + numCells
+                    swapCells()
                 }
             })
         }
@@ -92,14 +94,115 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    fun candySwap(){
-        var background1 : Int = candy.get(candyToBeReplaced).tag as Int
-        var background2 : Int = candy.get(candyToBeDragged).tag as Int
+    fun swapCells(){
+        var background1 : Int = cell.get(cellToBeReplaced).tag as Int
+        var background2 : Int = cell.get(cellToBeDragged).tag as Int
 
-        candy.get(candyToBeDragged).setImageResource(background1)
-        candy.get(candyToBeReplaced).setImageResource(background2)
+        cell.get(cellToBeDragged).setImageResource(background1)
+        cell.get(cellToBeReplaced).setImageResource(background2)
 
-        candy.get(candyToBeDragged).setTag(background1)
-        candy.get(candyToBeReplaced).setTag(background2)
+        cell.get(cellToBeDragged).setTag(background1)
+        cell.get(cellToBeReplaced).setTag(background2)
     }
+
+    private fun moveDownCells(){
+        val firstRow = arrayOf(1,2,3,4,5,6,7,8)
+        val list = asList(*firstRow)
+        for (i in 55 downTo 0){
+            if (cell.get(i + numCells).tag as Int == emptyCell){
+
+                cell.get(i + numCells).setImageResource(cell.get(i).tag as Int)
+                cell.get(i + numCells).setTag(cell.get(i).tag as Int)
+
+                cell.get(i).setImageResource(emptyCell)
+                cell.get(i).setTag(emptyCell)
+            }
+        }
+    }
+
+    // *************** SCORE CHECKING ***************
+
+    //  ROWS
+    private fun checkRowForThree(){
+        for( i in 0..61){
+            var chosenCandy = cell.get(i).tag
+
+            var isBlank : Boolean = cell.get(i).tag == emptyCell
+            val notValid = arrayOf(6,7,14,15,22,23,30,31,38,39,46,47,54,55)
+
+            val list = asList(*notValid)
+
+            if(!list.contains(i)){
+                var x = i
+
+                if(cell.get(x++).tag as Int == chosenCandy
+                    && !isBlank
+                    && cell.get(x++).tag as Int == chosenCandy
+                    && cell.get(x).tag as Int == chosenCandy
+                ){
+                    score += 3
+                    scoreResult.text = "$score"
+                    cell.get(x).setImageResource(emptyCell)
+                    cell.get(x).setTag(emptyCell)
+                    x--
+                    cell.get(x).setImageResource(emptyCell)
+                    cell.get(x).setTag(emptyCell)
+                    x--
+                    cell.get(x).setImageResource(emptyCell)
+                    cell.get(x).setTag(emptyCell)
+                }
+            }
+
+        }
+        moveDownCells()
+    }
+
+    private fun checkRowForFour(){
+
+    }
+
+    private fun checkRowForFive(){
+
+    }
+
+    // COLUMNS
+    private fun checkColumnForThree(){
+        for( i in 0..47){
+            var chosenCandy = cell.get(i).tag
+
+            var isBlank : Boolean = cell.get(i).tag == emptyCell
+
+            var x = i
+            if(cell.get(x++).tag as Int == chosenCandy
+                && !isBlank
+                && cell.get(x+numCells).tag as Int == chosenCandy
+                && cell.get(x+2*numCells).tag as Int == chosenCandy
+            ){
+                score += 3
+                scoreResult.text = "$score"
+                cell.get(x).setImageResource(emptyCell)
+                cell.get(x).setTag(emptyCell)
+                x += numCells
+                cell.get(x).setImageResource(emptyCell)
+                cell.get(x).setTag(emptyCell)
+                x += numCells
+                cell.get(x).setImageResource(emptyCell)
+                cell.get(x).setTag(emptyCell)
+            }
+        }
+        moveDownCells()
+    }
+
+    private fun checkColumnForFour(){
+
+    }
+
+    private fun checkColumnForFive(){
+
+    }
+
+    // SPECIAL PATTERNS
+
+
+
 }
